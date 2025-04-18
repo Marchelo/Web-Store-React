1a74de365417697b7cfb5acaee10a91eb0328f72
Luka Drobnjak <159907553+Marchelo@users.noreply.github.com>
2025-04-18 19:20:10 +0200

    addToCart feature
    Adding functionality addToCart feature...

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,47 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},      
+    cartItems: [],
+    addItemToCart: () => {},
+    cartCount: 0   
+
 })
+
+const addCartItem = (cartItems, productToAdd) => {
+                                        // find if cartItems contains productToAdd
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    ); 
+                                        // if found increment quantity      (if that product exists in cart then put one more)
+    if(existingCartItem){   
+        return cartItems.map((cartItem)=>
+            cartItem.id === productToAdd.id                     
+            ? {...cartItem, quantity: cartItem.quantity + 1}         
+            : cartItem
+        );
+    }
+                                        // return new array with modified cartItems / new cartItem
+    return [...cartItems, { ...productToAdd, quantity: 1 }];    // new product
+}
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};  
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+    
+    useEffect(()=>{  
+                // accumulator startValue = 0 in every pass it sum previous total number of items in cart and that new update
+        const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0)   
+        setCartCount(newCartCount);
+    }, [cartItems])
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};  
 
     return <CartContext.Provider value={value}>{ children }</CartContext.Provider>
 }
