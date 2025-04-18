401824b31da4fd671351c3c9ff954f1124da7e23
Luka Drobnjak <159907553+Marchelo@users.noreply.github.com>
2025-04-18 22:13:25 +0200

    Checkout page
    Just created checkout page with checkout items and also checkout cart functionality.

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -5,8 +5,10 @@ export const CartContext = createContext({
     setIsCartOpen: () => {},      
     cartItems: [],
     addItemToCart: () => {},
-    cartCount: 0   
-
+    removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
+    cartCount: 0,
+    cartTotal: 0
 })
 
 const addCartItem = (cartItems, productToAdd) => {
@@ -26,22 +28,65 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];    // new product
 }
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+                                // find cartItem to remove
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );                                 
+                                // check if quantity is - if it is remove item from cart
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);   // if this statement is true keep value, else
+    }                                
+                                // else return back cartItems with matching cart item with reduced quantity(number) 3 > 2
+    return cartItems.map((cartItem)=>
+        cartItem.id === cartItemToRemove.id                     
+        ? {...cartItem, quantity: cartItem.quantity - 1}    // give me new object with same props except quantity: give me quantity reduced by 1        
+        : cartItem
+    );
+}
+
+// if the cartItemToClear is equal to cartItem (item which is already there) filter it out completly  
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
     
-    useEffect(()=>{  
+    useEffect(()=>{  // count
                 // accumulator startValue = 0 in every pass it sum previous total number of items in cart and that new update
         const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0)   
         setCartCount(newCartCount);
+    }, [cartItems])
+
+    useEffect(()=>{    // total
+        const newCartTotal = cartItems.reduce(
+            (total, cartItem)=> total + cartItem.quantity * cartItem.price, 0)   
+        setCartTotal(newCartTotal);
     }, [cartItems])
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};  
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    }
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear));
+    }
+
+    const value = {
+        isCartOpen, 
+        setIsCartOpen, 
+        addItemToCart,
+        removeItemToCart,
+        clearItemFromCart, 
+        cartItems, 
+        cartCount,
+        cartTotal
+    };  
 
     return <CartContext.Provider value={value}>{ children }</CartContext.Provider>
 }
